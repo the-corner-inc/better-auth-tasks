d427e9ffba4d7ec39bd8456b92a2a6dd0e2e5877
elias <borrajo.elias@gmail.com>
2026-01-04 21:53:10 +0100

    chore(config): Add initial drizzle configuration

--- better-auth-tasks/drizzle.config.ts
+++ better-auth-tasks/drizzle.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
 
-// Define and export the Drizzle Kit configuration for database schema generation and migrations
-// We define : The schema file, output directory for migrations, database dialect, and connection credentials
+// Configuration file that is used by Drizzle Kit and contains all the information about :
+// - your database connection,
+// - migration folder and
+// - schema files.
+// Define and export the Drizzle Kit configuration for database schema and migrations
 export default defineConfig({
     schema: './src/drizzle/schema.ts',              // Schemas to be used for migrations and generation
     out: './src/drizzle/migrations',                // Output directory for generated migration files
