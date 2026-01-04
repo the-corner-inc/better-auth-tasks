e906f37f8e88a81c1834dd727efcc90c93cffd61
elias <borrajo.elias@gmail.com>
2026-01-04 17:02:15 +0100

    chore(config): Add shadcn for the ui

--- better-auth-tasks/src/app/page.tsx
+++ better-auth-tasks/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import {useState} from "react";
 import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
 // PAGE : The main content of a specific route or URL. It changes based on navigation.
 
@@ -13,12 +14,12 @@ export default function Home() {
         <div className="my-6 px-4 max-w-md mx-auto">
             <div className="text-center space-y-6">
                 <h1 className="text-3xl font-bold">Welcome to Our App</h1>
-                <h1>COUNTER : {count}</h1>
-                <button onClick={() => setCount(c => c + 1)}
-                        className="dark:invert">
-                    +1
-                </button>
-                <Link href="/auth/login">Sign In / Sign Up</Link>
+                <Button asChild size="lg" onClick={() => setCount(c => c + 1)}>
+                    <h1>COUNTER : {count}</h1>
+                </Button>
+                <Button asChild size="lg">
+                    <Link href="/auth/login">Sign In / Sign Up</Link>
+                </Button>
             </div>
         </div>
   );
