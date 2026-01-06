d5548595f9e481d09c9a989c8e53072ff11e5150
elias <borrajo.elias@gmail.com>
2026-01-06 15:59:45 +0100

    chore(config): auth redirects, from login page if already logged in

--- better-auth-tasks/src/app/auth/login/page.tsx
+++ better-auth-tasks/src/app/auth/login/page.tsx
@@ -1,11 +1,26 @@
+"use client"
+
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {SignUpTab} from "@/app/auth/login/_components/sign-up-tab";
 import {SignInTab} from "@/app/auth/login/_components/sign-in-tab";
 import {Separator} from "@/components/ui/separator";
 import {SocialAuthButtons} from "@/app/auth/login/_components/social-auth-buttons";
+import {useEffect} from "react";
+import {authClient} from "@/lib/auth-client";
+import {useRouter} from "next/navigation";
 
 export default function LoginPage () {
+
+    // Don't show the sign in / sign up page if the user is already authenticated
+    const router = useRouter()
+    useEffect(() => {
+        authClient.getSession().then(session => {
+            if (session.data != null)
+                router.push("/")
+        })
+    })
+
     return (
         <Tabs defaultValue="signin" className="max-auto w-full my-6 px-4">
             <TabsList>
